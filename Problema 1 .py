2d417822b17f60b8413bfa7c45043971909eef83
im24230679 <L24230679@minatitlan.tecnm.mx>
2024-11-27 18:06:42 -0600

    Create Problema 1 .py

---
+++ Problema 1 .py
@@ -0,0 +1,9 @@
+# 1.- Fibonacci inverso: Implementa una función que calcule los primeros N números de la serie de Fibonacci en orden inverso.
+
+def fibonacci_inverso(n):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[-1] + fib[-2])
+    return fib[::-1]
+
+print(fibonacci_inverso(10))
